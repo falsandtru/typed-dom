faed6bd20c9e9e03adaf0e8728d4e801abbb87fe
falsandtru <falsandtru@users.noreply.github.com>
2016-05-02 22:29:04 +0900

    test(integration): refactor tests

--- test/integration/typedhtml.ts
+++ test/integration/typedhtml.ts
@@ -1,39 +1,62 @@
 import TypedHTML from 'typed-dom';
 
 describe('Integration: TypedHTML', function () {
-  describe('usage', function () {
+  describe('spec', function () {
     it('struct', function () {
-      const struct = TypedHTML.div({
+      const struct = TypedHTML.article({
         title: TypedHTML.h1(),
-        list: TypedHTML.ul([TypedHTML.li()])
+        content: TypedHTML.p()
       });
-      assert(struct.raw.nodeName === 'DIV');
-      assert(struct.raw.firstChild === struct.contents.title.raw);
+      assert(struct.raw.nodeName === 'ARTICLE');
+      assert(struct.contents.title.raw === struct.raw.firstChild);
       assert(struct.contents.title.raw.nodeName === 'H1');
-      assert(struct.contents.list.raw.nodeName === 'UL');
-      assert(struct.contents.list.contents[0].raw.nodeName === 'LI');
+      assert(struct.contents.content.raw.nodeName === 'P');
+    });
 
+    it('struct contents update', function () {
+      const struct = TypedHTML.article({
+        title: TypedHTML.h1()
+      });
+      struct.contents = {
+        title: TypedHTML.h2()
+      };
+      assert(struct.raw.children[0].nodeName === 'H2');
+      assert(struct.contents.title.raw === struct.raw.children[0]);
+    });
+
+    it('struct contents partial update', function () {
+      const struct = TypedHTML.article({
+        title: TypedHTML.h1()
+      });
       struct.contents.title = TypedHTML.h2();
       assert(struct.raw.children[0].nodeName === 'H2');
-      assert(struct.contents.title.raw.nodeName === 'H2');
-      struct.contents = {
-        title: TypedHTML.h1(),
-        list: TypedHTML.ul([TypedHTML.li()])
-      };
-      assert(struct.raw.children[0].nodeName === 'H1');
-      assert(struct.contents.title.raw.nodeName === 'H1');
+      assert(struct.contents.title.raw === struct.raw.children[0]);
     });
 
     it('list', function () {
-      const list = TypedHTML.p([TypedHTML.a()]);
-      assert(list.raw.nodeName === 'P');
-      assert(list.contents[0].raw.nodeName === 'A');
-      assert(list.contents[0].contents[0] === void 0);
-      assert(list.contents[1] === void 0);
+      const list = TypedHTML.ul([
+        TypedHTML.li(),
+        TypedHTML.li()
+      ]);
+      assert(list.raw.nodeName === 'UL');
+      assert(list.raw.children[0].nodeName === 'LI');
+      assert(list.contents[0].raw === list.raw.children[0]);
+      assert(list.raw.children[1].nodeName === 'LI');
+      assert(list.contents[1].raw === list.raw.children[1]);
+      assert(list.contents[2] === void 0 && list.raw.children[2] === void 0);
+    });
 
-      list.contents = [TypedHTML.a(), TypedHTML.a()];
-      assert(list.raw.children[1].nodeName === 'A');
-      assert(list.contents[1].raw.nodeName === 'A');
+    it('list contents update', function () {
+      const list = TypedHTML.ul([
+        TypedHTML.li(),
+        TypedHTML.li()
+      ]);
+      list.contents = [
+        TypedHTML.li()
+      ];
+      assert(list.raw.children[0].nodeName === 'LI');
+      assert(list.contents[0].raw === list.raw.children[0]);
+      assert(list.contents[1] === void 0 && list.raw.children[1] === void 0);
     });
 
   });
