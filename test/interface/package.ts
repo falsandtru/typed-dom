88be4e2c8b51db700f6464ea1000cca53523c53e
falsandtru <falsandtru@users.noreply.github.com>
2016-11-21 08:14:10 +0900

    feat(package): add interop with es6 modules

--- test/interface/package.ts
+++ test/interface/package.ts
@@ -1,6 +1,17 @@
-import TypedHTML from 'typed-dom';
+import TypedHTML, { __esModule } from 'typed-dom';
+
+declare module 'typed-dom' {
+  export const __esModule: boolean | undefined;
+}
 
 describe('Interface: Package', function () {
+  describe('module', function () {
+    it('module', function () {
+      assert(__esModule === true);
+    });
+
+  });
+
   describe('TypedHTML', function () {
     it('TypedHTML', function () {
       assert(typeof TypedHTML === 'object');
