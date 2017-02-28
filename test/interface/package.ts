3de5ad81fd5e76d455737b0f85e3fba4d23b7a1a
falsandtru <falsandtru@users.noreply.github.com>
2017-02-28 22:00:42 +0900

    chore(config): update dependencies

--- test/interface/package.ts
+++ test/interface/package.ts
@@ -1,17 +1,6 @@
-import TypedHTML, { __esModule } from 'typed-dom';
-
-declare module 'typed-dom' {
-  export const __esModule: boolean | undefined;
-}
+import TypedHTML from 'typed-dom';
 
 describe('Interface: Package', function () {
-  describe('module', function () {
-    it('module', function () {
-      assert(__esModule === true);
-    });
-
-  });
-
   describe('TypedHTML', function () {
     it('TypedHTML', function () {
       assert(typeof TypedHTML === 'object');
