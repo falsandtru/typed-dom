b6dd8652787949e40e020ea3748b673b7b9bffb5
falsandtru <falsandtru@users.noreply.github.com>
2018-03-08 07:24:14 +0900

    refactor(util): refactoring

--- src/util/dom.ts
+++ src/util/dom.ts
@@ -5,22 +5,24 @@ const cache = new Map<string, Element>();
 export function html<T extends keyof HTMLElementTagNameMap>(tag: T, children?: Node[] | string): HTMLElementTagNameMap[T];
 export function html<T extends keyof HTMLElementTagNameMap>(tag: T, attrs?: Record<string, string>, children?: Node[] | string): HTMLElementTagNameMap[T];
 export function html<T extends keyof HTMLElementTagNameMap>(tag: T, attrs: Record<string, string> | Node[] | string = {}, children: Node[] | string = []): HTMLElementTagNameMap[T] {
-  return element('html', () => document.createElement(tag), tag, attrs, children) as HTMLElementTagNameMap[T];
+  return element('html', tag, attrs, children);
 }
 
 export function svg<T extends keyof SVGElementTagNameMap_>(tag: T, children?: Node[] | string): SVGElementTagNameMap_[T];
 export function svg<T extends keyof SVGElementTagNameMap_>(tag: T, attrs?: Record<string, string>, children?: Node[] | string): SVGElementTagNameMap_[T];
 export function svg<T extends keyof SVGElementTagNameMap_>(tag: T, attrs: Record<string, string> | Node[] | string = {}, children: Node[] | string = []): SVGElementTagNameMap_[T] {
-  return element('svg', () => document.createElementNS("http://www.w3.org/2000/svg", tag), tag, attrs, children) as SVGElementTagNameMap_[T];
+  return element('svg', tag, attrs, children);
 }
 
-function element(ns: string, factory: () => Element, tag: string, attrs: Record<string, string> | Node[] | string = {}, children: Node[] | string = []): Element {
-  if (typeof children === 'string') return element(ns, factory, tag, attrs as {}, [document.createTextNode(children)]);
-  if (typeof attrs === 'string' || Array.isArray(attrs)) return element(ns, factory, tag, {}, attrs);
+function element<T extends keyof HTMLElementTagNameMap>(ns: 'html', tag: T, attrs?: Record<string, string> | Node[] | string, children?: Node[] | string): HTMLElementTagNameMap[T];
+function element<T extends keyof SVGElementTagNameMap_>(ns: 'svg', tag: T, attrs?: Record<string, string> | Node[] | string, children?: Node[] | string): SVGElementTagNameMap_[T];
+function element(ns: string, tag: string, attrs: Record<string, string> | Node[] | string = {}, children: Node[] | string = []): Element {
+  if (typeof children === 'string') return element(ns as 'html', tag as 'html', attrs as {}, [document.createTextNode(children)]);
+  if (typeof attrs === 'string' || Array.isArray(attrs)) return element(ns as 'html', tag as 'html', {}, attrs);
   const key = `${ns}:${tag}`;
   const el = cache.has(key)
     ? cache.get(key)!.cloneNode(true) as Element
-    : cache.set(key, factory()).get(key)!.cloneNode(true) as Element;
+    : cache.set(key, elem(ns, tag)).get(key)!.cloneNode(true) as Element;
   assert(el !== cache.get(key));
   assert(el.attributes.length === 0);
   assert(el.childNodes.length === 0);
@@ -31,6 +33,17 @@ function element(ns: string, factory: () => Element, tag: string, attrs: Record<
     .forEach(child =>
       void el.appendChild(child));
   return el;
+}
+
+function elem(ns: string, tag: string): Element {
+  switch (ns) {
+    case 'html':
+      return document.createElement(tag);
+    case 'svg':
+      return document.createElementNS("http://www.w3.org/2000/svg", tag);
+    default:
+      throw new Error(`TypedDOM: Unknown namespace: ${ns}`);
+  }
 }
 
 export const currentTargets = new WeakMap<Event, EventTarget>();
