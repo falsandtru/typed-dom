3e5cee68227f4b8d8635d229f8247aa87fdd0b04
falsandtru <falsandtru@users.noreply.github.com>
2018-05-21 03:28:35 +0900

    refactor: refactoring

--- src/dom/builder.ts
+++ src/dom/builder.ts
@@ -1,14 +1,14 @@
 import { El, ElChildren } from './manager';
-import { Factory as DefaultFactory, html, svg, define } from '../util/dom';
+import { Factory, html, svg, define } from '../util/dom';
 import { ExtractProp } from 'spica/type';
 
 interface ElBuilder<T extends string, E extends Element> {
-  (factory?: (f: Factory<E>) => E): El<T, E, ElChildren.Void>;                                                   <C extends ElChildren>
-  (children: C, factory?: (f: Factory<E>) => E): El<T, E, C>;
-  (attrs: Record<string, string | EventListener>, factory?: (f: Factory<E>) => E): El<T, E, ElChildren.Void>;    <C extends ElChildren>
-  (attrs: Record<string, string | EventListener>, children: C, factory?: (f: Factory<E>) => E): El<T, E, C>;
+  (factory?: (f: HelperFactory<E>) => E): El<T, E, ElChildren.Void>;                                                   <C extends ElChildren>
+  (children: C, factory?: (f: HelperFactory<E>) => E): El<T, E, C>;
+  (attrs: Record<string, string | EventListener>, factory?: (f: HelperFactory<E>) => E): El<T, E, ElChildren.Void>;    <C extends ElChildren>
+  (attrs: Record<string, string | EventListener>, children: C, factory?: (f: HelperFactory<E>) => E): El<T, E, C>;
 }
-interface Factory<E extends Element> {
+interface HelperFactory<E extends Element> {
   (children?: Iterable<Node> | string): E;
   (attrs?: Record<string, string | EventListener>, children?: Iterable<Node> | string): E;
 }
@@ -16,8 +16,8 @@ interface Factory<E extends Element> {
 export type API<T> = {
   readonly [P in Extract<keyof ExtractProp<T, Element>, string>]: T[P] extends Element ? ElBuilder<P, T[P]> : never;
 };
-export function API<T extends API<any>>(factory: DefaultFactory<Element>): T {
-  return new Proxy({} as T, handle(El, factory));
+export function API<T extends API<any>>(factory: Factory<Element>): T {
+  return new Proxy({} as T, handle(factory));
 }
 
 type TypedHTML = API<HTMLElementTagNameMap>;
@@ -26,20 +26,20 @@ export const TypedHTML: TypedHTML = API(html);
 type TypedSVG = API<SVGElementTagNameMap_>;
 export const TypedSVG: TypedSVG = API(svg);
 
-function handle<T extends object>(Elem: typeof El, defaultFactory: DefaultFactory<Element>): ProxyHandler<T> {
+function handle<T extends object>(factory: Factory<Element>): ProxyHandler<T> {
   return {
     get: (obj, prop) =>
       obj[prop] || typeof prop !== 'string'
         ? obj[prop]
-        : obj[prop] = builder(prop, (...args: any[]) => defaultFactory(prop, ...args)),
+        : obj[prop] = builder(prop, (...args: any[]) => factory(prop, ...args)),
   };
 
-  function builder<E extends Element, C extends ElChildren>(tag: string, defaultFactory: Factory<E>): (attrs?: Record<string, string | EventListener>, children?: C, factory?: () => E) => El<string, E, C> {
+  function builder<E extends Element, C extends ElChildren>(tag: string, helperFactory: HelperFactory<E>): (attrs?: Record<string, string | EventListener>, children?: C, factory?: () => E) => El<string, E, C> {
     return function build(attrs?: Record<string, string | EventListener>, children?: C, factory?: () => E): El<string, E, C> {
       if (typeof attrs === 'function') return build(undefined, undefined, attrs);
       if (typeof children === 'function') return build(attrs, undefined, children);
       if (attrs !== undefined && isChildren(attrs)) return build(undefined, attrs, factory);
-      return new Elem(elem(tag, factory || (() => defaultFactory()), attrs), children as C);
+      return new El(elem(tag, factory || (() => helperFactory()), attrs), children!);
     };
 
     function isChildren(children: C | Record<string, string | EventListener>): children is C {
@@ -47,8 +47,8 @@ function handle<T extends object>(Elem: typeof El, defaultFactory: DefaultFactor
           || Object.values(children as object).slice(-1).every(val => typeof val === 'object');
     }
 
-    function elem(tag: string, factory: (f: Factory<E>) => E, attrs?: Record<string, string | EventListener>): E {
-      const el = factory(defaultFactory);
+    function elem(tag: string, factory: (f: HelperFactory<E>) => E, attrs?: Record<string, string | EventListener>): E {
+      const el = factory(helperFactory);
       if (tag !== el.tagName.toLowerCase()) throw new Error(`TypedDOM: Tag name must be "${tag}", but got "${el.tagName.toLowerCase()}".`);
       attrs && void define(el, attrs);
       return el;
