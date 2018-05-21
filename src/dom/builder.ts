945af01ff48458880fbac86466dae4be6714dc09
falsandtru <falsandtru@users.noreply.github.com>
2018-05-21 10:27:47 +0900

    refactor: refactoring

--- src/dom/builder.ts
+++ src/dom/builder.ts
@@ -1,34 +1,34 @@
 import { El, ElChildren } from './manager';
-import { Factory as HelperFactory, html, svg, define } from '../util/dom';
+import { Factory as BaseFactory, html, svg, define } from '../util/dom';
 import { ExtractProp } from 'spica/type';
 
-interface ElBuilder<T extends string, E extends Element, F extends HelperFactory<Element>> {
+interface ElBuilder<T extends string, E extends Element, F extends BaseFactory<Element>> {
   (factory?: Factory<F, T, E>): El<T, E, ElChildren.Void>;                                                   <C extends ElChildren>
   (children: C, factory?: Factory<F, T, E>): El<T, E, C>;
   (attrs: Record<string, string | EventListener>, factory?: Factory<F, T, E>): El<T, E, ElChildren.Void>;    <C extends ElChildren>
   (attrs: Record<string, string | EventListener>, children: C, factory?: Factory<F, T, E>): El<T, E, C>;
 }
-type Factory<F extends HelperFactory<Element>, T extends string, E extends Element> = (factory: F, tag: T) => E;
+type Factory<F extends BaseFactory<Element>, T extends string, E extends Element> = (baseFactory: F, tag: T) => E;
 
-export type API<T extends object, F extends HelperFactory<Element>> = {
+export type API<T extends object, F extends BaseFactory<Element>> = {
   readonly [P in Extract<keyof ExtractProp<T, Element>, string>]: T[P] extends Element ? ElBuilder<P, T[P], F> : never;
 };
-export function API<T extends object, F extends HelperFactory<Element>>(helperFactory: F): API<T, F> {
-  return new Proxy({} as API<T, F>, handle(helperFactory));
+export function API<T extends object, F extends BaseFactory<Element>>(baseFactory: F): API<T, F> {
+  return new Proxy({} as API<T, F>, handle(baseFactory));
 }
 
 export const TypedHTML: API<HTMLElementTagNameMap, typeof html> = API(html);
 export const TypedSVG: API<SVGElementTagNameMap_, typeof svg> = API(svg);
 
-function handle<T extends object, F extends HelperFactory<Element>>(helperFactory: F): ProxyHandler<T> {
+function handle<T extends object, F extends BaseFactory<Element>>(baseFactory: F): ProxyHandler<T> {
   return {
     get: (obj, prop) =>
       obj[prop] || typeof prop !== 'string'
         ? obj[prop]
-        : obj[prop] = builder(prop, helperFactory),
+        : obj[prop] = builder(prop, baseFactory),
   };
 
-  function builder<E extends Element, C extends ElChildren>(tag: string, helperFactory: F): (attrs?: Record<string, string | EventListener>, children?: C, factory?: () => E) => El<string, E, C> {
+  function builder<E extends Element, C extends ElChildren>(tag: string, baseFactory: F): (attrs?: Record<string, string | EventListener>, children?: C, factory?: () => E) => El<string, E, C> {
     return function build(attrs?: Record<string, string | EventListener>, children?: C, factory?: Factory<F, string, E>): El<string, E, C> {
       if (typeof attrs === 'function') return build(undefined, undefined, attrs);
       if (typeof children === 'function') return build(attrs, undefined, children);
@@ -42,7 +42,7 @@ function handle<T extends object, F extends HelperFactory<Element>>(helperFactor
     }
 
     function elem(factory: Factory<F, string, E>, attrs?: Record<string, string | EventListener>): E {
-      const el = factory(helperFactory, tag);
+      const el = factory(baseFactory, tag);
       if (tag !== el.tagName.toLowerCase()) throw new Error(`TypedDOM: Tag name must be "${tag}", but got "${el.tagName.toLowerCase()}".`);
       attrs && void define(el, attrs);
       return el;
