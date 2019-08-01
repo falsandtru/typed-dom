19c4b59c95d9787b07b1eedac0e8ac7d0c72fe87
falsandtru <falsandtru@users.noreply.github.com>
2019-08-01 23:50:47 +0900

    refactor: refactoring

--- src/util/query.ts
+++ src/util/query.ts
@@ -1,10 +1,12 @@
 import { Attrs, define } from "./dom";
 
-export function apply<T extends keyof HTMLElementTagNameMap>(node: ParentNode, selector: T, attrs?: Attrs): Iterable<HTMLElementTagNameMap[T]>
-export function apply<T extends keyof SVGElementTagNameMap_>(node: ParentNode, selector: T, attrs?: Attrs): Iterable<SVGElementTagNameMap_[T]>
-export function apply<T extends Element = Element>(node: ParentNode, selector: string, attrs?: Attrs): Iterable<T>
-export function* apply<T extends Element = Element>(node: ParentNode, selector: string, attrs?: Attrs): Iterable<T> {
-  for (const n of node.querySelectorAll<T>(selector)) {
-    yield define(n, attrs);
+export function apply<T extends keyof HTMLElementTagNameMap>(node: ParentNode, selector: T, attrs?: Attrs): NodeListOf<HTMLElementTagNameMap[T]>
+export function apply<T extends keyof SVGElementTagNameMap_>(node: ParentNode, selector: T, attrs?: Attrs): NodeListOf<SVGElementTagNameMap_[T]>
+export function apply<T extends Element = Element>(node: ParentNode, selector: string, attrs?: Attrs): NodeListOf<T>
+export function apply<T extends Element = Element>(node: ParentNode, selector: string, attrs?: Attrs): NodeListOf<T> {
+  const ns = node.querySelectorAll<T>(selector);
+  for (const n of ns) {
+    void define(n, attrs);
   }
+  return ns;
 }
