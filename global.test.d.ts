8e253d4e85797649050adfab9698c21ec750dd92
falsandtru <falsandtru@users.noreply.github.com>
2019-10-31 17:51:10 +0900

    chore: update dependencies

--- global.test.d.ts
+++ global.test.d.ts
@@ -1,9 +1,9 @@
 import assert from 'power-assert';
-import i18next from 'i18next';
+import { i18n } from 'i18next';
 
 type Assert = typeof assert;
 
 declare global {
   const assert: Assert;
-  const i18next: i18next.i18n;
+  const i18next: i18n;
 }
