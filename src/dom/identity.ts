454e07e2a4adbf5392b3446b1c77dd325316e7ad
falsandtru <falsandtru@users.noreply.github.com>
2020-03-10 00:26:57 +0900

    Refactoring

--- src/dom/identity.ts
+++ src/dom/identity.ts
@@ -1,8 +1,8 @@
 import { uuid } from 'spica/uuid';
-import { sqid } from 'spica/sqid';
 
 const id = uuid().slice(-7);
+let counter = 0;
 
 export function uid(): string {
-  return `id-${id}-${+sqid()}`;
+  return `id-${id}-${++counter}`;
 }
