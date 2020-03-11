9ef31fbf5a370da42f0b937f5508cf006e2e6fe0
falsandtru <falsandtru@users.noreply.github.com>
2020-03-12 03:14:46 +0900

    Refactoring

--- src/util/listener.ts
+++ src/util/listener.ts
@@ -90,7 +90,9 @@ export function bind<T extends keyof WindowEventMap | keyof DocumentEventMap | k
 
   function handler(ev: Event): unknown {
     assert(ev.currentTarget);
-    ev[currentTarget] = ev.currentTarget;
-    return listener(ev);
+    return currentTarget in ev
+      ? undefined
+      : ev[currentTarget] = ev.currentTarget,
+      listener(ev);
   }
 }
