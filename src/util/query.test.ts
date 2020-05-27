de13825ee257748155df97fb3e77faa7fc54aeca
falsandtru <falsandtru@users.noreply.github.com>
2020-05-27 16:13:42 +0900

    Refactoring

--- src/util/query.test.ts
+++ src/util/query.test.ts
@@ -5,7 +5,7 @@ describe('Unit: util/query', () => {
   describe('apply', () => {
     it('', () => {
       assert.deepStrictEqual(
-        [...apply(html('div', [html('br'), html('br')]), 'br')].map(el => el.outerHTML),
+        [...apply(html('div', [html('br'), html('br')]), 'br', {})].map(el => el.outerHTML),
         ['<br>', '<br>']);
       assert.deepStrictEqual(
         [...apply(html('div', [html('br'), html('br')]), 'br:not([id])', { id: '' })].map(el => el.outerHTML),
--- src/util/query.ts
+++ src/util/query.ts
@@ -1,9 +1,9 @@
 import { Attrs, define } from './dom';
 
-export function apply<T extends keyof HTMLElementTagNameMap>(node: ParentNode, selector: T, attrs?: Attrs): NodeListOf<HTMLElementTagNameMap[T]>
-export function apply<T extends keyof SVGElementTagNameMap>(node: ParentNode, selector: T, attrs?: Attrs): NodeListOf<SVGElementTagNameMap[T]>
-export function apply<T extends Element = Element>(node: ParentNode, selector: string, attrs?: Attrs): NodeListOf<T>
-export function apply<T extends Element = Element>(node: ParentNode, selector: string, attrs?: Attrs): NodeListOf<T> {
+export function apply<T extends keyof HTMLElementTagNameMap>(node: ParentNode, selector: T, attrs: Attrs): NodeListOf<HTMLElementTagNameMap[T]>
+export function apply<T extends keyof SVGElementTagNameMap>(node: ParentNode, selector: T, attrs: Attrs): NodeListOf<SVGElementTagNameMap[T]>
+export function apply<T extends Element = Element>(node: ParentNode, selector: string, attrs: Attrs): NodeListOf<T>
+export function apply<T extends Element = Element>(node: ParentNode, selector: string, attrs: Attrs): NodeListOf<T> {
   const ns = node.querySelectorAll<T>(selector);
   for (let i = 0, len = ns.length; i < len; ++i) {
     define(ns[i], attrs);
