f1d7de7d1a847f251737639645c5a264778dda98
falsandtru <falsandtru@users.noreply.github.com>
2020-12-27 23:16:11 +0900

    Update dependencies

--- src/util/identity.ts
+++ src/util/identity.ts
@@ -1,6 +1,6 @@
-import { global } from 'spica/global';
+import { global, Set } from 'spica/global';
 import { rnd0Z, unique } from 'spica/random';
 
 const ids = Symbol.for('typed-dom::ids');
-export const identity = unique(rnd0Z, 2, global[ids] ??= Object.create(null));
+export const identity = unique(rnd0Z, 2, global[ids] ??= new Set());
 assert(global[ids]);
