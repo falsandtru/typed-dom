af964019f945184751d9498037e4b70c053c0bc3
falsandtru <falsandtru@users.noreply.github.com>
2022-09-06 07:07:47 +0900

    Refactoring

--- src/util/query.ts
+++ src/util/query.ts
@@ -4,7 +4,7 @@ import { duffReduce } from 'spica/duff';
 export function querySelectorWith<T extends keyof HTMLElementTagNameMap>(node: ParentNode, selector: T): HTMLElementTagNameMap[T] | null;
 export function querySelectorWith<T extends keyof SVGElementTagNameMap>(node: ParentNode, selector: T): SVGElementTagNameMap[T] | null;
 export function querySelectorWith<T extends string>(node: ParentNode, selector: T): ParseSelector<T>;
-export function querySelectorWith<T extends Element = Element>(node: ParentNode, selector: string): T | null;
+export function querySelectorWith<T extends Element>(node: ParentNode, selector: string): T | null;
 export function querySelectorWith(node: ParentNode | Element, selector: string): Element | null {
   return 'matches' in node && node.matches(selector)
     ? node
@@ -14,7 +14,7 @@ export function querySelectorWith(node: ParentNode | Element, selector: string):
 export function querySelectorAllWith<T extends keyof HTMLElementTagNameMap>(node: ParentNode, selector: T): HTMLElementTagNameMap[T][];
 export function querySelectorAllWith<T extends keyof SVGElementTagNameMap>(node: ParentNode, selector: T): SVGElementTagNameMap[T][];
 export function querySelectorAllWith<T extends string>(node: ParentNode, selector: T): ParseSelector<T>[];
-export function querySelectorAllWith<T extends Element = Element>(node: ParentNode, selector: string): T[];
+export function querySelectorAllWith<T extends Element>(node: ParentNode, selector: string): T[];
 export function querySelectorAllWith(node: ParentNode | Element, selector: string): Element[] {
   const acc: Element[] = [];
   if ('matches' in node && node.matches(selector)) {
@@ -26,7 +26,7 @@ export function querySelectorAllWith(node: ParentNode | Element, selector: strin
 export function querySelectorAll<T extends keyof HTMLElementTagNameMap>(node: ParentNode, selector: T): HTMLElementTagNameMap[T][];
 export function querySelectorAll<T extends keyof SVGElementTagNameMap>(node: ParentNode, selector: T): SVGElementTagNameMap[T][];
 export function querySelectorAll<T extends string>(node: ParentNode, selector: T): ParseSelector<T>[];
-export function querySelectorAll<T extends Element = Element>(node: ParentNode, selector: string): T[];
+export function querySelectorAll<T extends Element>(node: ParentNode, selector: string): T[];
 export function querySelectorAll(node: ParentNode | Element, selector: string): Element[] {
   return duffReduce(node.querySelectorAll(selector), (acc, node) => (acc.push(node), acc), [] as Element[]);
 }
