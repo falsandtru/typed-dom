38bde19a3432c48551eac56b7606848e48320a36
falsandtru <falsandtru@users.noreply.github.com>
2022-09-15 14:55:02 +0900

    Update dependencies

--- src/util/identity.ts
+++ src/util/identity.ts
@@ -1,14 +1,6 @@
-import { global } from 'spica/global';
+import { counter } from 'spica/counter';
 
 // uniqueによる実装はメモリを圧迫しqueryのベンチマークが落ちるため不可
-const rnd = (c => () => (c++).toString(36))(0);
-const dict = [...Array(36)].map((_, i) => i.toString(36)).join('');
-assert(dict.length === 36);
-assert(dict[0] === '0');
-assert(dict.at(-1) === 'z');
-let r = '';
-let c = dict.length - 1;
-
 export const identity: () => string = global[Symbol.for('typed-dom::identity')]
-  ??= () => `${++c === dict.length ? r = rnd() : r}${dict[c = c % dict.length]}`;
-assert(/^\w0$/.test(identity()));
+  ??= counter(36);
+assert([...Array(100)].every((_, i) => identity() === (++i).toString(36)));
