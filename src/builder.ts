62ae8f8433e74036b57a7892a463e8775ffc52b2
falsandtru <falsandtru@users.noreply.github.com>
2023-04-15 21:28:14 +0900

    Refactoring

--- src/builder.ts
+++ src/builder.ts
@@ -7,7 +7,7 @@ export type API
   BuilderFunction<M> & { readonly [P in K<M>]: BuilderMethod<M, P>; };
 export function API
   <M extends TagNameMap>
-  (baseFactory: Factory<M>, container?: (el: E<M[K<M>]>) => ShadowRoot)
+  (baseFactory: Factory<M>, container?: (el: E<M[K<M>]>) => ShadowRoot | undefined)
   : API<M> {
   return new Proxy<API<M>>((() => 0) as any, handle(baseFactory, container));
 }
@@ -64,7 +64,7 @@ interface BuilderMethod<M extends TagNameMap, T extends K<M>> {
 
 function handle
   <M extends TagNameMap>
-  (baseFactory: Factory<M>, container?: (el: Element) => ShadowRoot,
+  (baseFactory: Factory<M>, container?: (el: Element) => ShadowRoot | undefined,
 ): ProxyHandler<API<M>> {
   return {
     apply(target, _, [tag, ...args]) {
