9fb046d39aeaf2ec79a0fa1f70c0e89a7f1849b7
falsandtru <falsandtru@users.noreply.github.com>
2023-07-27 21:35:12 +0900

    Refactoring

--- src/util/identity.ts
+++ src/util/identity.ts
@@ -3,5 +3,4 @@ import { counter } from 'spica/counter';
 
 // uniqueによる実装はメモリを圧迫しqueryのベンチマークが落ちるため不可
 export const identity: () => string = global[Symbol.for('typed-dom::identity')]
-  ??= counter(36);
-assert([...Array(100)].every((_, i) => identity() === (++i).toString(36)));
+  ??= counter(62);
